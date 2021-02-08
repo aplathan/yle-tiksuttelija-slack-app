09bafc49d4b50b8465be362efac07c2619a7e752
Antti Plathan <antti.plathan@gmail.com>
2021-02-08 14:34:31 +0200

    Eka commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# yle-ttv-slack-app
+
+This Yle TTV Slack App is Based on "Getting Started ⚡️ Bolt for JavaScript tutorial. This is a private project that has no ties with Yleisradio Oy with the exception that is uses Yle's public API to fetch Yle Teksti-TV pages. The Yle in the project name simply implies that instead of being a generic Teletext app this can only be used to view comtent from Yle.
+
+> Slack app example from 📚 [Getting started with Bolt for JavaScript tutorial][1]
+
+## Overview
+
+This is a Slack app built with the [Bolt for JavaScript framework][2]. When deployed with the included configuration, a CloudFormation Stack is created including the main app as an AWS Lambda Function, API Gateway for the requests from Slack, and CloudWatch Logs for monitoring the app.
+
+## Installing
+
+### 1. Setup environment variables
+
+Before using the app, you must get Yle's API keys from [Yle Developer Site][5]. Read and accept Yle's API license terms, and [order keys for Yle External API][6] (registration needed).
+
+```zsh
+# Replace with your signing secret and token
+export SLACK_BOT_TOKEN=<your-bot-token>
+export SLACK_SIGNING_SECRET=<your-signing-secret>
+export TTV_API_KEY=<your Yle API keys>
+```
+
+### 2. Setup your local project and Serverless Framework
+
+```zsh
+# Clone this project onto your machine
+git clone https://github.com/aplathan/yle-ttv-slack-app.git
+
+# Change into the project
+cd yle-ttv-slack-app/
+
+# Install the dependencies
+npm install
+```
+
+[Install Serverless][7] with your preferred method. Note that running Serverless with Node (npx serverless deploy) is very slow compared to running it netively (serverless deploy).
+
+### 3. Testing locally
+
+Before deploying yle-ttv-slack-app to AWS it may be a good idea to test it locally to mamke sure everything works fine with your Slack workspace.
+
+```zsh
+# Start the local deck
+serverless offline --noPrependStageInUrl
+
+# If you installed Serverless with npm, say
+npx serverless offline --noPrependStageInUrl
+```
+
+[Setup ngrok][3] to create a local requests URL for development.
+
+```zsh
+npm run ngrok
+npm run start
+```
+
+Set up a new Slack app in your Slack workspace and copy the url given by ngrok to your Slack app's Slash Commands page. Create New Command if you haven't done it before. Append /slack/events to the ngrok url.
+
+```zsh
+# Tell Slack where to send events when your app is invoked
+https://<randomly-generated-hostname>.ngrok.io/slack/events
+```
+
+## Deploying to AWS via CloudFormation
+
+The sls deploy command deploys your entire service via CloudFormation. Run this command when you have made infrastructure changes (i.e., you edited serverless.yml). Use serverless deploy function -f myFunction when you have made code changes and you want to quickly upload your updated code to AWS Lambda or just change function configuration.
+
+## Contributing
+
+### Issues and questions
+
+Found a bug or have a question about this project? We'd love to hear from you!
+
+1. Browse to [aplathan/yle-ttv-slack-app/issues][4]
+1. Create a new issue
+1. Select the `[x] examples` category
+
+See you there and thanks for helping to improve Bolt for everyone!
+
+[1]: https://slack.dev/bolt-js/tutorial/getting-started
+[2]: https://slack.dev/bolt-js
+[3]: https://slack.dev/bolt-js/tutorial/getting-started#setting-up-events
+[4]: https://github.com/aplathan/yle-ttv-slack-app/issues/new
+[5]: https://developer.yle.fi/en/index.html
+[6]: https://tunnus.yle.fi/api-avaimet
+[7]: https://www.serverless.com/framework/docs/getting-started/
---
+++ app.js
@@ -0,0 +1,150 @@
+// yle-tiksuttelija-slack-app
+// Antti Plathan 6 Feb 2021
+// antti.plathan@gmail.com
+
+const { App, ExpressReceiver } = require('@slack/bolt');
+const awsServerlessExpress = require('aws-serverless-express');
+
+// Exposataan custom http routerin parserit (json...)
+// https://github.com/slackapi/bolt-js/issues/516
+const express = require('express')
+
+
+// Initialize your custom receiver
+const expressReceiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  // The `processBeforeResponse` option is required for all FaaS environments.
+  // It allows Bolt methods (e.g. `app.message`) to handle a Slack request
+  // before the Bolt framework responds to the request (e.g. `ack()`). This is
+  // important because FaaS immediately terminate handlers after the response.
+  processBeforeResponse: true
+});
+
+// Initializes your app with your bot token and the AWS Lambda ready receiver
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver: expressReceiver
+});
+
+// Initialize your AWSServerlessExpress server using Bolt's ExpressReceiver
+const server = awsServerlessExpress.createServer(expressReceiver.app);
+
+// Luodaan dictionary sivujen hakusanoista ja niiden mäppäylsistä TTV-sivunmeroihin
+var pageDict = {
+  "etusivu": 100, 
+  "hakemistot": 199,
+  "kotimaa": 102,
+  "ulkomaat": 130,
+  "talous": 160,
+  "sää": 400,
+  "liikenne": 400,
+  "urheilu": 201,
+  "nhl": 235,
+  "änäri": 235,
+  "eurojalkapallo": 600,
+  "jalkapallo": 600,
+  "fudis": 600,
+  "veikkaus": 470,
+  "tv-ohjelmat": 300,
+  "tv": 300,
+  "ohjelmat": 300,
+  "ohjelmaopas": 350,
+  "opas": 350,
+  "alueuutiset": 500,
+  "news": 190,
+  "english": 190,
+  "newsinenglish": 190,
+  "svenska": 700,
+  "påsvenska": 700,
+  "viikkomakasiini": 800,
+  "viikko": 800,
+  "makasiini": 800
+};
+
+app.command('/tiksu', async ({ command, ack, say, client, body }) => {
+  await ack();
+
+  console.log(command);
+  console.log(command.text);
+
+  // Parsitaan käyttäjän antamasta komennosta vain ensimmäinen parametri
+  const requestedPage = (command.text).split(' ')[0];
+  console.log(requestedPage);
+  // ttvPage pysyy falsena ellei pyynnöst löydy validia sivupyyntöä
+  var ttvPage = false;
+
+  // Löytyykö pyydetty sivu dictionarystä? Samalla se tarkoittaa myös sitä, että pyyntö ei ollut numero
+  if (requestedPage in pageDict) {
+    console.log("Sivu löytyy dictionarystä! Asetetaan näytettäväksi TTV-sivuksi:");
+    // Haetaan dictionarystä pyydettyä osastoa vastaava ttv-sivunumero
+    ttvPage = pageDict[requestedPage]; 
+    console.log(ttvPage);
+  } else if (isNaN(requestedPage)) {
+      // Jos pyyntö ei ole numero (ei ole suoraa testiä sille, että ON numero), ttvPage pysyy falsena ja käyttäjälle tullaan näyttämään helppi
+      console.log("Pyydetty sivu ei ole numero eikä kuulu tunnettujen osastojen joukkoon:");
+      console.log(requestedPage);
+      ttvPage = false; // Redundantti, mutta luettavuuden helpottamiseksi
+    } else {
+        // Nyt tiedetään, että pyyntö oli numero. Muutetaan se kokonaisluvuksi
+        const pageNumber = parseInt(requestedPage, 10);
+
+        // Ylen TTV:n sallitut sivunumerot ovat 100-899
+        if (pageNumber > 99 && pageNumber < 900) {
+          console.log("Sivunumero on sallituissa rajoissa.");
+          ttvPage = pageNumber; // Asetetaan haettava TTV-sivunumero
+        } else {
+          console.log("Sivunumero ei ole sallituissa rajoissa!");
+          ttvPage = false; // Redundantti, mutta luettavuuden helpottamiseksi
+        }
+      }
+     
+  
+  if (ttvPage) {
+    // Näytettävä sivu tiedetään, yritetään näyttää se
+    console.log("Perillä ollaan, yritetään näyttää pyydetty sivunumero: ");
+    console.log(ttvPage);
+
+    await say({
+      text: `https://external.api.yle.fi/v1/teletext/images/${ttvPage}/1.png?${process.env.TTV_API_KEY}`,
+      replace_original: false,
+      response_type: `ephemeral`,
+      unfurl_links: true,
+      unfurl_media: true
+    });
+  } else {
+    // Kelvollista TTV-sivua ei voitu asettaa, näytetään helppi käyttäjälle
+    console.log("Loppuun päädyttiin, mutta käyttäjän syötteestä ei tunnistettu oikeaa sivunumeroa tai osastoa, tai pyyntö oli help:")
+    console.log(ttvPage);
+
+    await say({
+      response_type: 'ephemeral',
+      text: 'Näin käytät /ttv -komentoa:',
+      attachments:[
+          {
+              text:'Anna haettavan TTV-sivun numero `/ttv 100` tai osaston nimi `/ttv kotimaa`.\nOsastojen nimet ovat `etusivu`, `hakemistot`, `kotimaa`, `ulkomaat`, `talous`, `sää`, `liikenne`, `urheilu`, `nhl`, `eurojalkapallo`, `veikkaus`, `tv-ohjelmat`, `ohjelmaopas`, `alueuutiset`, `news`, `svenska`, `viikkomakasiini`.\n'
+          }]
+    });    
+  }
+
+  
+
+
+});  
+
+
+
+
+
+// Tämä ei ole nyt käytössä, muista poistaa 
+// Listening get requests from external sources, mainly for testing
+expressReceiver.router.get('/test', (req, res) => {
+  // You're working with an express req and res now.
+  res.send('yay, get!');
+});
+
+
+// Handle the Lambda function event
+module.exports.handler = (event, context) => {
+  console.log('⚡️ Bolt app is running!');
+  awsServerlessExpress.proxy(server, event, context);
+};
